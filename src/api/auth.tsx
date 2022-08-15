487acfa595983f4db1ffdde4088fd8851f9d9e21
Nguyễn Ngọc Anh <anhnn02.it@gmail.com>
2022-08-15 20:34:19 +0700

    first commit

--- src/api/auth.tsx
+++
@@ -1,8 +0,0 @@
-import instance from "./instance"
-
-export const login = (data) => {
-    return instance.post("/login", data) 
-}
-export const register = (data) => {
-    return instance.post("/signup", data) 
-}
